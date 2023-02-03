50cb7b922bdea14b72694adeab1542a1b6bfe428
zeyn13 <znplika@gmail.com>
2023-02-03 21:08:26 +0100

    server

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const bcrpyt = require("bcrpyt");
+const cors = require("cors");
+const User = require("./models/User");
 
 const app = express();
-app.get("/", (req, res, next) => {
-  res.send("hello");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//routes
+app.post("/signup", (req, res, next) => {
+  const newUser = new User({
+    email: req.body.email,
+    name: req.body.name,
+    password: bcrpyt.hashSync(req.body.password, 10),
+  });
+  console.log(newUser);
 });
 
 const port = process.env.PORT || 5000;
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -8,7 +8,35 @@
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "Signup",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      error: "",
+    };
+  },
+  methods: {
+    signup() {
+      let newUser = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      };
+      axios.post("http://localhost:5000/signup", newUser).then(
+        (res) => {
+          this.error = "";
+          this.$router.push("/login");
+        },
+        (err) => {
+          console.log(err.response);
+          this.error = err.response.data.error;
+        }
+      );
+    },
+  },
 };
 </script>
