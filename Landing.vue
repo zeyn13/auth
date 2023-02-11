952bf7b342526f124cda508318a4429a44fe461e
Jehona123 <101560137+Jehona123@users.noreply.github.com>
2023-02-11 20:36:56 +0100

    Create Landing.vue

---
+++ Landing.vue
@@ -0,0 +1,32 @@
+<template>
+ <div>
+  <button @click="logout >logout</button>
+  </div>
+<.template>
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Landing',
+  created() {
+    //user is not authorized
+    if (localStorange.getItem('token') === null) {
+      this.Srounter.push('/login');
+   }
+ },
+ mounted() {
+ axios.get('http://localhost:5000/user', { headers: { token: localStorage.getItem('token') {
+ .then(res => {
+  console.log(res);
+ })
+ },
+ methods: {
+  logout() {
+  localStorange.clear{};
+  this.Srouter.push('/login');
+}
+</script>
+
+
+
+
