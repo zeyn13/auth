ecc43c872fc15b8235a39830544d6ece545b2dff
Jehona123 <101560137+Jehona123@users.noreply.github.com>
2023-02-11 20:15:10 +0100

    Create Login.vue

---
+++ Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  EMAIL: <input type="text" v-model="email"> <br/>
+  PASSWORD: <input tybe="text" v-model="password"> <br/>
+  <button> @click="login">login</button>
+ </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Login',
+  data() {
+  return {
+   email: '',
+   password: '',
+   }
+ },
+ methods: {
+   login() {
+    let user = {
+    email: this.email,
+    password: this.password
+  }
+    axios.post('http://localhost:5000/login', user)
+    .then(res => {
+   console.log(res);
+   })
+  }
+ }
+} 
+</script>
